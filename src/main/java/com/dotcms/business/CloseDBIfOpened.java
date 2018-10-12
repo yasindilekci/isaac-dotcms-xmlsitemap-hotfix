cc421268e369501a36b9419cac6760695d88a184
ISAAC\yasin.dilekci <yasin.dilekci@isaac.nl>
2018-10-12 08:52:47 +0200

    [TASK] Initial commit

---
+++ src/main/java/com/dotcms/business/CloseDBIfOpened.java
@@ -0,0 +1,24 @@
+package com.dotcms.business;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A method annotated with CloseDBIfOpened annotation will close resources in the current thread if needed, such as database connections...
+ * @author jsanca
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface CloseDBIfOpened {
+
+    /**
+     * By default in true, set to false if you do not want to close the connection hold on the current thread
+     * @return boolean
+     */
+    boolean connection() default true;
+
+} // E:O:F:LogTime
